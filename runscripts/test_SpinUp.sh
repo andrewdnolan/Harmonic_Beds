30734654a7063225492540281cea82ef5fc7d6de
andrewdnolan (westgrid) <andrew.d.nolan@maine.edu>
2021-04-29 07:37:30 -0700

    forgot to load the environment in the test serial script

--- runscripts/test_SpinUp.sh
+++ runscripts/test_SpinUp.sh
@@ -6,6 +6,24 @@
 #SBATCH --mail-user=andrew.d.nolan@maine.edu       # email to spend updates too
 # in the previous two lines %A" is replaced by jobID and "%a" with the array index
 
+###############################################################################
+#######################            Load ENV             #######################
+###############################################################################
+# Load Elmer
+module load gcc/9.3.0
+module load elmerfem/scc20
+
+# Load python 3
+module load python/3.6
+
+# Creating virtual environments inside of your jobs
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+
+pip install --no-index --upgrade pip
+pip install --no-index -r $HOME/requirements.txt
+###############################################################################
+
 # Get the fp to a .sif file
 SIF=$(  sed -n "1p" Inputs.txt)
 # Get the corresponding python call to clean surface boundary data
