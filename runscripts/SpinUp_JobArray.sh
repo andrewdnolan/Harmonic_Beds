7f5738169e2cbdf9b4dbed45f027ae7bede7706c
andrewdnolan (westgrid) <andrew.d.nolan@maine.edu>
2021-05-14 13:53:17 -0700

    There's a problem with loading the python env. Trying something lazy where I just load the default py4elmer env in the /home/anolan dir

--- runscripts/SpinUp_JobArray.sh
+++ runscripts/SpinUp_JobArray.sh
@@ -2,7 +2,7 @@
 #SBATCH --array=1-11                               # 10 jobs that run 10 at a time
 #SBATCH --job-name=SpinUp_2000a_dt_1_dx_100_       # base job name for the array
 #SBATCH --mem-per-cpu=150                          # maximum 150M per job
-#SBATCH --time=1:30:00                             # maximum walltime per job
+#SBATCH --time=1:00:00                             # maximum walltime per job
 #SBATCH --nodes=1                                  # Only one node is needed
 #SBATCH --ntasks=1                                 # These are serial jobs
 #SBATCH --mail-type=ALL                            # send all mail (way to much)
@@ -21,12 +21,18 @@ module load elmerfem/9.0
 # Load python 3
 module load python/3.6
 
-# Creating virtual environments inside of your jobs
-virtualenv --no-download $SLURM_TMPDIR/env
-source $SLURM_TMPDIR/env/bin/activate
+# Lazy way
+source $HOME/py4elmer/env/bin/activate
 
-pip install --no-index --upgrade pip
-pip install --no-index -r $HOME/requirements.txt
+#////////////////////
+## Fancy way 
+#////////////////////
+# Creating virtual environments inside of your jobs
+# virtualenv --no-download $SLURM_TMPDIR/env
+# source $SLURM_TMPDIR/env/bin/activate
+
+# pip install --no-index --upgrade pip
+# pip install --no-index -r $HOME/requirements.txt
 ###############################################################################
 
 # Get the fp to a .sif file
