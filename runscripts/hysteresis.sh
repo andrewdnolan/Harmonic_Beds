3bb99d1e4af44f62004c9fa71066319ffe09e992
andrewdnolan <andrew.d.nolan@maine.edu>
2021-05-18 18:38:18 -0400

    update hysterisis script to be better written. Think we are ready to go for a spin on Westgrid

--- runscripts/hysteresis.sh
+++ runscripts/hysteresis.sh
@@ -8,115 +8,191 @@
 
 set +x
 
-BED="Data/Topography/pert_r_0.01_harmonics_1-10.dat"
+EXP='hystersis'                                         # Experiment (folder) name
+BED='farinotti_corrected'                               # Mesh DB for the given bed config
 SIF='./SRC/SIFs/Hysterisis_norestart.sif'               # Template SIF FILE
+BED_FP="Data/Topography/pert_r_0.01_harmonics_1-10.dat"
 
 
-NT=3001                                 # Number of time step
-dt=1                                    # size of timestep
-TT=$((NT*dt-dt))                        # total time of simulation
+NT=3001                                                 # Number of time step
+dt=1                                                    # size of timestep
+TT=$((NT*dt-dt))                                        # total time of simulation
 
-if [ ! -d "Synthetic/Hystersis" ]; then
-  mkdir Synthetic/Hystersis
+
+DOCKER=true
+WESTGRID=false
+
+# Make top dir if it does not exist
+if [ ! -d "Synthetic/${BED}/${EXP}" ]; then
+  mkdir Synthetic/${BED}/${EXP}
+fi
+# Clean the input  file so we can create a new one with commands specifc
+if [ -f Inputs.txt ]; then
+  rm -f Inputs.txt
+fi
+# Clean the output file so we can create a new one with commands specifc
+if [ -f Outputs.txt ]; then
+  rm -f Outputs.txt
 fi
 
 # perturb the bed with first 10 harmonics of the series
 # (i.e. evaluate the series from 1 to 10)
 python3 ./SRC/utils/make_bed.py \
         -B Data/Topography/REF_BedTopo_2.dat \
-        -O $BED \
-        -H 215.0 \
-        -N 10    \
-        -R 0.01  \
+        -O $BED_FP \
+        -H 200.0   \
+        -N 10      \
+        -R 0.01    \
         --sum
 
 
-for RUN in "observed_IC" "smoothBed_SS_IC"; do
+for IC in "observed_IC" "smoothBed_SS_IC"; do
 
-  if [ ! -d "Synthetic/Hystersis/${RUN}" ]; then
-    mkdir Synthetic/Hystersis/${RUN}
+  # Set up neccessary directory structure and make mesh
+  if [ ! -d "Synthetic/${BED}/Hystersis/${IC}" ]; then
+
+    mkdir "Synthetic/${BED}/Hystersis/${IC}"            # Make dir for IC
+    mkdir "Synthetic/${BED}/Hystersis/${IC}/SaveData"   # Elmer .dat files folders
+    mkdir "Synthetic/${BED}/Hystersis/${IC}/VTU"        # folder for VTU files
+    mkdir "Synthetic/${BED}/Hystersis/${IC}/hdf5"       # folder for .nc files
+
+    if [ "$DOCKER" = true ]; then
+      # Make mesh within Docker environment
+      docker exec elmerenv /bin/sh -c "cd /home/glacier/shared_directory/Synthetic; \
+                                       ./Mesh/makemsh.sh Synthetic/${BED}/Hystersis/${IC}/mesh_dx100 100"
+    else
+      # Make mesh driectly
+      ./Mesh/makemsh.sh Synthetic/${BED}/Hystersis/${IC}/mesh_dx100 100
+    fi
+
   fi
 
-  if [ ! -e "Synthetic/Hystersis/${RUN}/mesh" ]; then
-    # Execute the .SIF file via "ElmerSolver"
-    docker exec elmerenv /bin/sh -c "cd /home/glacier/shared_directory/Synthetic; \
-                                     bash Mesh/makemsh.sh Synthetic/Hystersis/${RUN}/mesh"
+  # Get the approriate SIF template
+  if [ "$IC" = "observed_IC" ]; then
+    SIF="./SRC/SIFs/Hysterisis_norestart.sif"
+  elif [ "$IC" = "smoothBed_SS_IC" ]; then
+    SIF="./SRC/SIFs/Hysterisis_restart.sif"
+  else
+    echo "Invalid Initial Condition" && exit 1
   fi
+
+
+  for OFFSET in $(seq -w 1.90 0.01 2.10);do
+    echo
+    echo "--------------------------------------------------------------------------"
+    echo "% Running ${IC} with ${OFFSET} offset"
+    echo "--------------------------------------------------------------------------"
+    echo
+
+    RUN="${IC}_${TT}a_dt_${dt}_dx_100_mb_${OFFSET}_off"
+    OUT_FP="${BED}/${EXP}/${IC}"
+
+    # Update the .SIF FILE with the model run specifc params
+    sed "s#<NT>#"$NT"#g;
+        s#<dt>#"$dt"#g;
+        s#<RUN>#"$RUN"#g;
+        s#<BED_FP>#"$BED_FP"#g;
+        s#<OUT_FP>#"$OUT_FP"#g;
+        s#<OFFSET>#"$OFFSET"#g" "${SIF}" > "./SRC/SIFs/${RUN}.sif"
+
+
+    echo "./SRC/SIFs/${RUN}.sif"  >> Inputs.txt
+
+    # # Execute the .SIF file via "ElmerSolver"
+    # docker exec elmerenv /bin/sh -c "cd /home/glacier/shared_directory/Synthetic; \
+    #                                 ElmerSolver ./SRC/SIFs/${RUN}.sif \
+    #                                 | tee ./logs/Hystersis/Observed_IC/${RUN}.log"
+
+    # Clean the boundary data and convert from .dat to .nc
+    echo python3 ./SRC/utils/dat2h5.py \
+            ./Synthetic/${OUT_FP}/SaveData/${RUN}.dat \
+            -out_dir ./Synthetic/${OUT_FP}/hdf5 \
+            -Nx 279 \
+            -dt $dt \
+            -offset $OFFSET \
+            -SpinUp >> Outputs.txt
+
+    # Remove the edited SIF to reduce clutter
+    # rm ./SRC/SIFs/${RUN}.sif
+  done
 done
 
-for OFFSET in $(seq -w 2.00 0.01 2.50);do
-  echo
-  echo "--------------------------------------------------------------------------"
-  echo "% Running Observed_IC with ${OFFSET} offset"
-  echo "--------------------------------------------------------------------------"
-  echo
 
-  RUN="observed_${TT}a_dt_${dt}_dx_100_mb_${OFFSET}_off"
-  EXP='observed_ic'
-  BED='hystersis'
-
-  # Update the .SIF FILE with the model run specifc params
-  sed 's#^$NT = [^ ]*#$NT = '"${NT}"'#;
-       s#^$dt = [^ ]*#$dt = '"${dt}"'#;
-       s#^\$RUN = [^ ]*#\$RUN = "'"${RUN}"'"#;
-       s#^\$BED = [^ ]*#\$BED = "'"${BED}"'"#;
-       s#^\$EXP = [^ ]*#\$EXP = "'"${EXP}"'"#;
-       s#^$offset = [^ ]*#\$offset = '"${OFFSET}"'#;' ./SRC/SIFs/Hysterisis_norestart.sif > "./SRC/SIFs/${RUN}.sif"
-
-  echo "./SRC/SIFs/${RUN}.sif"  >> Inputs.txt
-
-  # # Execute the .SIF file via "ElmerSolver"
-  # docker exec elmerenv /bin/sh -c "cd /home/glacier/shared_directory/Synthetic; \
-  #                                 ElmerSolver ./SRC/SIFs/${RUN}.sif \
-  #                                 | tee ./logs/Hystersis/Observed_IC/${RUN}.log"
-
-  # Clean the boundary data and convert from .dat to .nc
-  echo python3 ./SRC/utils/dat2h5.py \
-          ./Synthetic/${BED}/observed_ic/SaveData/${RUN}.dat \
-          -out_dir ./Synthetic/${BED}/observed_ic/hdf5 \
-          -Nx 284 \
-          -dt $dt \
-          -offset $OFFSET \
-          -SpinUp >> Outputs.txt
-
-  # Remove the edited SIF to reduce clutter
-  # rm ./SRC/SIFs/${RUN}.sif
-
-
-  echo
-  echo "--------------------------------------------------------------------------"
-  echo "% Running SmoothBed_SS_IC with ${OFFSET} offset"
-  echo "--------------------------------------------------------------------------"
-  echo
-
-  RUN="smoothbed_${TT}a_dt_${dt}_dx_100_mb_${OFFSET}_off"
-  EXP='smoothbed_ss_ic'
-  BED='hystersis'
-
-  # Update the .SIF FILE with the model run specifc params
-  sed 's#^$NT = [^ ]*#$NT = '"${NT}"'#;
-       s#^$dt = [^ ]*#$dt = '"${dt}"'#;
-       s#^\$RUN = [^ ]*#\$RUN = "'"${RUN}"'"#;
-       s#^\$BED = [^ ]*#\$BED = "'"${BED}"'"#;
-       s#^\$EXP = [^ ]*#\$EXP = "'"${EXP}"'"#;
-       s#^$offset = [^ ]*#\$offset = '"${OFFSET}"'#;' ./SRC/SIFs/Hysterisis_restart.sif > "./SRC/SIFs/${RUN}.sif"
-
-  echo "./SRC/SIFs/${RUN}.sif"  >> Inputs.txt
-
-  # # Execute the .SIF file via "ElmerSolver"
-  # docker exec elmerenv /bin/sh -c "cd /home/glacier/shared_directory/Synthetic; \
-  #                                 ElmerSolver ./SRC/SIFs/${RUN}.sif \
-  #                                 | tee ./logs/Hystersis/SmoothBed_SS_IC/${RUN}.log"
-
-  # Clean the boundary data and convert from .dat to .nc
-  echo python3 ./SRC/utils/dat2h5.py \
-          ./Synthetic/${BED}/smoothbed_ss_ic/SaveData/${RUN}.dat \
-          -out_dir ./Synthetic/${BED}/smoothbed_ss_ic/hdf5 \
-          -Nx 284 \
-          -dt $dt \
-          -offset $OFFSET \
-          -SpinUp >> Outputs.txt
-
-  # Remove the edited SIF to reduce clutter
-  # rm ./SRC/SIFs/${RUN}.sif
-done
+# for OFFSET in $(seq -w 1.90 0.01 2.10);do
+#   echo
+#   echo "--------------------------------------------------------------------------"
+#   echo "% Running Observed_IC with ${OFFSET} offset"
+#   echo "--------------------------------------------------------------------------"
+#   echo
+#
+#   IC="observed_IC"
+#   RUN="${IC}_${TT}a_dt_${dt}_dx_100_mb_${OFFSET}_off"
+#   OUT_FP="${BED}/${EXP}/${IC}"
+#
+#   # Update the .SIF FILE with the model run specifc params
+#   sed "s#<NT>#"$NT"#g;
+#       s#<dt>#"$dt"#g;
+#       s#<RUN>#"$RUN"#g;
+#       s#<BED_FP>#"$BED_FP"#g;
+#       s#<OUT_FP>#"$OUT_FP"#g;
+#       s#<OFFSET>#"$OFFSET"#g" ./SRC/SIFs/Hysterisis_norestart.sif > "./SRC/SIFs/${RUN}.sif"
+#
+#
+#   echo "./SRC/SIFs/${RUN}.sif"  >> Inputs.txt
+#
+#   # # Execute the .SIF file via "ElmerSolver"
+#   # docker exec elmerenv /bin/sh -c "cd /home/glacier/shared_directory/Synthetic; \
+#   #                                 ElmerSolver ./SRC/SIFs/${RUN}.sif \
+#   #                                 | tee ./logs/Hystersis/Observed_IC/${RUN}.log"
+#
+#   # Clean the boundary data and convert from .dat to .nc
+#   echo python3 ./SRC/utils/dat2h5.py \
+#           ./Synthetic/${OUT_FP}/SaveData/${RUN}.dat \
+#           -out_dir ./Synthetic/${OUT_FP}/hdf5 \
+#           -Nx 284 \
+#           -dt $dt \
+#           -offset $OFFSET \
+#           -SpinUp >> Outputs.txt
+#
+#   # Remove the edited SIF to reduce clutter
+#   # rm ./SRC/SIFs/${RUN}.sif
+#
+#
+#   echo
+#   echo "--------------------------------------------------------------------------"
+#   echo "% Running SmoothBed_SS_IC with ${OFFSET} offset"
+#   echo "--------------------------------------------------------------------------"
+#   echo
+#
+#   IC="smoothBed_SS_IC"
+#   RUN="${IC}_${TT}a_dt_${dt}_dx_100_mb_${OFFSET}_off"
+#   OUT_FP="${BED}/${EXP}/${IC}"
+#
+#   # Update the .SIF FILE with the model run specifc params
+#   sed "s#<NT>#"$NT"#g;
+#       s#<dt>#"$dt"#g;
+#       s#<RUN>#"$RUN"#g;
+#       s#<BED_FP>#"$BED_FP"#g;
+#       s#<OUT_FP>#"$OUT_FP"#g;
+#       s#<OFFSET>#"$OFFSET"#g" ./SRC/SIFs/Hysterisis_restart.sif > "./SRC/SIFs/${RUN}.sif"
+#
+#
+#   echo "./SRC/SIFs/${RUN}.sif"  >> Inputs.txt
+#
+#   # # Execute the .SIF file via "ElmerSolver"
+#   # docker exec elmerenv /bin/sh -c "cd /home/glacier/shared_directory/Synthetic; \
+#   #                                 ElmerSolver ./SRC/SIFs/${RUN}.sif \
+#   #                                 | tee ./logs/Hystersis/SmoothBed_SS_IC/${RUN}.log"
+#
+#   # Clean the boundary data and convert from .dat to .nc
+#   echo python3 ./SRC/utils/dat2h5.py \
+#           ./Synthetic/${OUT_FP}/SaveData/${RUN}.dat \
+#           -out_dir ./Synthetic/${OUT_FP}/hdf5 \
+#           -Nx 284 \
+#           -dt $dt \
+#           -offset $OFFSET \
+#           -SpinUp >> Outputs.txt
+#
+#   # Remove the edited SIF to reduce clutter
+#   # rm ./SRC/SIFs/${RUN}.sif
+# done
