edd0fa2f9a22bb64af332954b8b14b87efb44722
andrewdnolan (westgrid) <andrew.d.nolan@maine.edu>
2021-07-06 17:10:42 -0700

    tweaked mem and walltime for S.S. 2000 a runs

--- elmer/runscripts/SpinUp_JobArray.sh
+++ elmer/runscripts/SpinUp_JobArray.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --array=1-16                               # 16 jobs that run 20 at a time
 #SBATCH --job-name=SpinUp_2000a_dt_1_dx_50_        # base job name for the array
-#SBATCH --mem-per-cpu=150                          # maximum 200M per job
-#SBATCH --time=1:00:00                             # maximum walltime per job
+#SBATCH --mem-per-cpu=400                          # maximum 200M per job
+#SBATCH --time=1:30:00                             # maximum walltime per job
 #SBATCH --nodes=1                                  # Only one node is needed
 #SBATCH --ntasks=1                                 # These are serial jobs
 #SBATCH --mail-type=ALL                            # send all mail (way to much)
