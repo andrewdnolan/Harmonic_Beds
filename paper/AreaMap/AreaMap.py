584c77551f661ba098cf515cf29b762bd47998da
andrewdnolan <andrew.d.nolan@maine.edu>
2022-03-15 13:27:40 -0700

    updated paper figures

--- paper/AreaMap/AreaMap.py
+++ paper/AreaMap/AreaMap.py
@@ -10,6 +10,7 @@ import fiona
 import cartopy
 import rasterio
 import numpy as np
+import xarray as xr
 import rasterio.mask
 import shapely.geometry
 import geopandas as gpd
@@ -43,16 +44,15 @@ def inset_map():
     AOI = gpd.read_file(AOI_fn)
 
     AEA = ccrs.AlbersEqualArea(
-        central_longitude=-145, central_latitude=60, standard_parallels=(55, 65)
+        central_longitude=-145, central_latitude=62.5, standard_parallels=(55, 65)
     )
 
     # Create a Stamen terrain background instance.
     stamen_terrain = cimgt.Stamen("terrain-background")
 
-    # sub_ax = plt.axes([0.705, 0.6075, 0.25, 0.25], projection=stamen_terrain.crs)
-    sub_ax = plt.axes([0.585, 0.575, 0.3, 0.3], projection=AEA)
-
-    sub_ax.set_extent([-170, -130, 55, 70], ccrs.Geodetic())
+    # sub_ax = plt.axes([0.585, 0.575, 0.3, 0.3], projection=AEA)
+    sub_ax = plt.axes([0.45, 0.575, 0.3, 0.3], projection=AEA)
+    sub_ax.set_extent([-150, -130, 55, 70], ccrs.Geodetic())
 
     grat  = cartopy.feature.NaturalEarthFeature(category="physical", scale="50m", name="graticules_5")
     land  = cartopy.feature.NaturalEarthFeature(category="physical", scale="50m", name="land")
@@ -92,49 +92,101 @@ def inset_map():
     sub_ax.annotate(
         "Study Area",
         xy=(AOI["geometry"][0].centroid.x, AOI["geometry"][0].centroid.y),
-        xytext=(-3e5, -3e5),
+        xytext=(-2e5, -7e5),
         textcoords="data",
         bbox=bbox_props,
         arrowprops=arrow_props,
-        size=5,
+        size=8,
         zorder=4,
     )
     sub_ax.annotate(
-        "Alaska",
-        xy=(-5e5, 3.5e5),
+        "AK",
+        xy=(-2e5, 0.5e5),
         textcoords="data",
         bbox=bbox_props,
-        size=5,
+        size=8,
         zorder=4,
     )
     sub_ax.annotate(
         "Yukon",
-        xy=(3e5, 3.5e5),
+        xy=(2.5e5, 0.5e5),
+        transform=AEA,
         textcoords="data",
         bbox=bbox_props,
-        size=5,
+        size=8,
         zorder=4,
     )
-    sub_ax.annotate(
-        "BC",
-        xy=(7.25e5, -1e5),
-        textcoords="data",
-        bbox=bbox_props,
-        size=5,
-        zorder=4,
-    )
+    # sub_ax.annotate(
+    #     "BC",
+    #     xy=(7.25e5, -1e5),
+    #     textcoords="data",
+    #     bbox=bbox_props,
+    #     size=5,
+    #     zorder=4,
+    # )
+
+def scale_bar(ax, proj, length, location=(0.5, 0.05), linewidth=3):
+    """
+    http://stackoverflow.com/a/35705477/1072212
+    ax is the axes to draw the scalebar on.
+    proj is the projection the axes are in
+    location is center of the scalebar in axis coordinates ie. 0.5 is the middle of the plot
+    length is the length of the scalebar in km.
+    linewidth is the thickness of the scalebar.
+    units is the name of the unit
+    m_per_unit is the number of meters in a unit
+    """
+
+    from matplotlib import patheffects
+
+    units='km'
+    m_per_unit=1000
+
+    # Get the extent of the plotted area in coordinates in metres
+    x0, x1, y0, y1 = ax.get_extent(proj)
+    # Turn the specified scalebar location into coordinates in metres
+    sbcx, sbcy = x0 + (x1 - x0) * location[0], y0 + (y1 - y0) * location[1]
+    # Generate the x coordinate for the ends of the scalebar
+    bar_xs = [sbcx - length * m_per_unit/2, sbcx + length * m_per_unit/2]
+    # buffer for scalebar
+    buffer = [patheffects.withStroke(linewidth=5, foreground="w")]
+    # Plot the scalebar with buffer
+    ax.plot(bar_xs, [sbcy, sbcy], transform=proj, color='k',
+        linewidth=linewidth, path_effects=buffer)
+    # buffer for text
+    buffer = [patheffects.withStroke(linewidth=3, foreground="w")]
+    # Plot the scalebar label
+    t0 = ax.text(sbcx, sbcy, str(length) + ' ' + units, transform=proj,
+        horizontalalignment='center', verticalalignment='bottom',
+        path_effects=buffer, zorder=2, size=12)
+
+    N_x = bar_xs[1] - length*m_per_unit*0.15
+    N_y = sbcy      + length*m_per_unit*0.15
+
+    plt.rc("text", usetex=False)
+    # Plot the N arrow
+    t1 = ax.text(N_x, N_y, u"\u25B2\nN", transform=proj,
+        horizontalalignment='center', verticalalignment='bottom',
+        path_effects=buffer, zorder=2, size=12)
+    plt.rc("text", usetex=True)
+
+    # Plot the scalebar without buffer, in case covered by text buffer
+    ax.plot(bar_xs, [sbcy, sbcy], transform=proj, color='k',
+        linewidth=linewidth, zorder=3)
 
 ################################################################################
 # Filepath decleration
 ################################################################################
-FL_dir = "/Users/andrewnolan/sfuvault/LilKluane/flowlines/"
-FL_fp  = os.path.join(FL_dir, "manual/surge_trib/surge_trib.shp")
-FL2_fp = os.path.join(FL_dir, "manual/main/Little_Kluan_FL_main.shp")
-AOI_fn = "/Users/andrewnolan/sfuvault/LilKluane/AOI/AOI.shp"
+LK_dir = "/Users/andrewnolan/sfuvault/LilKluane"
+FL_fp  = "./Data/flowlines/centerlines_postsurge.shp"
+AOI_fn = os.path.join(LK_dir, "AOI/AOI.shp")
 
-Baseimg_fn = "./S2_baseim_29_08_2018.tif"
-PreGLIMS_fn = "/Users/andrewnolan/sfuvault/LilKluane/GLIMS/Lil_Kluane/pre_surge/LilKluane_PreSurge.shp"
-PostGLIMS_fn = "/Users/andrewnolan/sfuvault/LilKluane/GLIMS/Lil_Kluane/post_surge/LilKluane_postsurge.shp"
+Baseimg_fn   = "./S2_baseim_29_08_2018.tif"
+PreGLIMS_fn  = os.path.join(LK_dir, "GLIMS/Lil_Kluane/pre_surge/LilKluane_PreSurge.shp")
+PostGLIMS_fn = os.path.join(LK_dir, "GLIMS/Lil_Kluane/post_surge/LilKluane_postsurge.shp")
+
+GPR_2019_fp = "./Data/Radar/2019/lk_gpr.shp"
+GPR_2021_fp = "./Data/Radar/2021/LK_18JUL2021_picked_line_*.nc"
 ################################################################################
 
 
@@ -157,23 +209,25 @@ with rasterio.open(Baseimg_fn) as src:
 
     transform = rasterio.windows.transform(sub_window, src.transform)
 
-flowline = gpd.read_file(FL_fp)
-# flowline2 = gpd.read_file(FL2_fp)
-
-ticks = to_points(flowline.loc[0, "geometry"], 10.0)  # 10 m
+flowline   = gpd.read_file(FL_fp)
+flowline   = flowline.to_crs(crs.proj4_init)
+ticks      = to_points(flowline.loc[0, "geometry"], 10.0)  # 10 m
 ticks["x"] = ticks.index.to_numpy() * 10
-ticks.crs = crs.proj4_init
 
-PreGLIMS = gpd.read_file(PreGLIMS_fn)
+PreGLIMS  = gpd.read_file(PreGLIMS_fn)
 PostGLIMS = gpd.read_file(PostGLIMS_fn)
 
-radar_lines = gpd.read_file('/Users/andrewnolan/Desktop/LittleKluane2021.kml', driver='KML')
+GPR_2019 = gpd.read_file(GPR_2019_fp)
+GPR_2019 = GPR_2019.to_crs(epsg=32607)
+GPR_2021 = xr.open_mfdataset(GPR_2021_fp, concat_dim='Line', combine="nested")
+
+radar_lines = gpd.read_file('./Data/Radar/LittleKluane2021.kml', driver='KML')
 radar_lines = radar_lines.to_crs(epsg=32607)
 
 ################################################################################
 # FIGURE
 ################################################################################
-fig = plt.figure(figsize=(5, 5))
+fig = plt.figure(figsize=(6, 4))
 ax  = plt.subplot(111, projection=crs)
 
 # Plot the base image
@@ -182,10 +236,21 @@ ax.imshow(base_img.transpose([1, 2, 0]), transform=crs, origin="upper", extent=i
 ax.add_geometries( PreGLIMS.geometry,  facecolor="none", crs=crs, edgecolor="#e7298a", linewidth=1.0)
 ax.add_geometries( PostGLIMS.geometry, facecolor="none", crs=crs, edgecolor="#7570b3", linewidth=1.0)
 ax.add_geometries( flowline.geometry,  facecolor="none", crs=crs, edgecolor="k",       linewidth=1.0)
-# plot the radar lines from 2021
-ax.add_geometries( radar_lines.geometry,  facecolor="none", crs=crs, edgecolor="#d95f02", linewidth=2.0)
-
 ticks.loc[ticks["x"] % 1000 == 0, "geometry"].plot( ax=ax, markersize=4, marker="x", zorder=4, color="k" )
+
+
+# plot the radar lines from 2019
+ax.scatter(GPR_2019.geometry.apply(lambda x: x.coords[0][0]),
+           GPR_2019.geometry.apply(lambda x: x.coords[0][1]),
+           color="#d95f02", s=5, zorder=6)
+
+# plot the radar lines from 2021
+for i in range(len(GPR_2021.Line)):
+
+    ax.scatter(GPR_2021.isel(Line=i).Easting,
+               GPR_2021.isel(Line=i).Northing,
+               color="#d95f02", s=5, zorder=6)
+
 
 ################################################################################
 # Legend
@@ -193,36 +258,44 @@ ticks.loc[ticks["x"] % 1000 == 0, "geometry"].plot( ax=ax, markersize=4, marker=
 patches = []
 patches.append(mpatches.Patch(edgecolor="#e7298a", facecolor="none", label="Pre-Surge" ))
 patches.append(mpatches.Patch(edgecolor="#7570b3", facecolor="none", label="Post-Surge"))
-patches.append(Line2D([0], [0], color="#d95f02", label="2021 Radar Lines"))
+patches.append(Line2D([0], [0], color="#d95f02", label="Radar data"))
 patches.append(Line2D([0], [0], color="k", label="Flowline"))
-ax.legend(handles=patches, loc="upper left", ncol=1, framealpha=1, edgecolor="black", labelspacing=0 )  # , fontsize = 'large'
+ax.legend(handles=patches,
+            loc="lower left",
+            ncol=2,
+            framealpha=1,
+            edgecolor="black",
+            labelspacing=0 )  # , fontsize = 'large'
 ################################################################################
+
 
 ################################################################################
 # Axis formating
 ################################################################################
 # Set the axis limits
-ax.set_ylim(6.748e6, 6.765e6)
+ax.set_ylim(6.748e6, 6.761e6) #6.765e6
 ax.set_xlim(5.700e5, 589280.0)
 
 ax.tick_params("y", rotation=300)
 ax.ticklabel_format(style="sci", scilimits=(-3, 4), axis="both")
 
-ax.yaxis.tick_right()
-ax.xaxis.set_visible(True)
-ax.yaxis.set_visible(True)
-ax.yaxis.set_label_position("right")
+# ax.yaxis.tick_right()
+# ax.xaxis.set_visible(True)
+# ax.yaxis.set_visible(True)
+# ax.yaxis.set_label_position("right")
 
-ax.set_xlabel("Easting (m)")
-ax.set_ylabel("Northing (m)", rotation=270, labelpad=15)
+# ax.set_xlabel("Easting (m)")
+# ax.set_ylabel("Northing (m)", rotation=270, labelpad=15)
 ################################################################################
 
 ################################################################################
 # Inset Map
-################################################################################
 inset_map()
+# Scale Bar
+scale_bar(ax, crs, 5, location=(0.85, 0.05))
 ################################################################################
 
-#plt.show()
-plt.savefig('geom_main_trib_test.eps',bbox_inches='tight', pad_inches=0.1, dpi=400)
-plt.close()
+# plt.tight_layout()
+# plt.show()
+plt.savefig('geom_main_trib_test.pdf', bbox_inches='tight')
+# plt.close()
--- paper/MB_gridsearch/MB_gridsearch.py
+++ paper/MB_gridsearch/MB_gridsearch.py
@@ -3,17 +3,54 @@
 import os
 import sys
 import glob
+import pickle
 import numpy as np
 import xarray as xr
 import pandas as pd
 import scipy.linalg as LA
 import matplotlib.pyplot as plt
 import scipy.interpolate as interpolate
-
 sys.path.append('../../elmer/')
 import SRC.utils.plot_spinup as plotting
 
-plt.rcParams['text.usetex'] = True
+# Set global matplotlib style parameters
+plt.rcParams.update({'font.size': 10,
+                     'text.usetex': True,
+                     'figure.facecolor': 'w',
+                     'savefig.bbox': 'tight'})
+
+################################################################################
+# EMY Data and SVR model loading
+################################################################################
+# Load the EMY data
+MB  = np.load('../../elmer/Data/SMB/EMY_data/surgio_withdeb_NMB.npy') # Annual SMB
+ZZ  = np.load('../../elmer/Data/SMB/EMY_data/surgio_Zs.npy')          # Elevation
+
+# Take temporal average of mb data and convert from mwe a^{-1} to m a^{-1}
+MB  = np.mean(MB,axis=0)*1000.0/910.    # Annual mean mass balance in [m a^{-1}]
+
+# Get rid of nans and outlier
+z   = ZZ[(~np.isnan(MB)) & ~(MB < -10)]
+b   = MB[(~np.isnan(MB)) & ~(MB < -10)]
+#return the indexes of the sorted arrays
+idx = z.argsort()
+#sort the arrays based on the elevation
+z   = z[idx].reshape(-1,1)
+b   = b[idx]
+
+
+# Load the trained serialized SVR
+pkl_fn = "../../elmer/Data/SMB/SVR/svr_model.pkl"
+with open(pkl_fn, 'rb') as file:
+    svr = pickle.load(file)
+
+# Do prediction with the trained SVR model
+b_hat = svr.predict(z)
+################################################################################
+
+################################################################################
+# Reference Geometry MB gridsearch loading
+################################################################################
 
 fp      = '../../elmer/Synthetic/exp_01_elevation_dependent/hdf5/LK_*dx_50*.nc'
 files   = sorted(glob.glob(fp))
@@ -23,26 +60,76 @@ for file in files:
     xarrays.append(xr.open_dataset(file))
 
 mf_dataset = xr.concat(xarrays,
-                       pd.Index(data = np.linspace(1.90, 2.05, 16), name='Delta_MB'))
+                       pd.Index(data = np.linspace(1.90, 2.05, 16),
+                                name = 'Delta_MB'))
 
-mf_dataset['z_s'] = mf_dataset.z_s.where((mf_dataset.z_s - mf_dataset.z_b) != 10., mf_dataset.z_b)
+mf_dataset['z_s'] = mf_dataset.z_s.where((mf_dataset.z_s - mf_dataset.z_b) != 10.,
+                                          mf_dataset.z_b )
 mf_dataset["H"]   = mf_dataset.z_s - mf_dataset.z_b
 
 Vol = mf_dataset.H.integrate("x") / mf_dataset.H.isel(t=1).integrate("x")
+################################################################################
 
+################################################################################
+# Figure creation
+################################################################################
+fig, ax = plt.subplots(1, 2, figsize=(8.5,3), constrained_layout=True)
+
+ax[0].scatter(z, b,
+           marker='o',
+           s=10,
+           alpha=0.5,
+           facecolor="none",
+           edgecolor='tab:blue',
+           label='Young and others (2021)')
+
+ax[0].plot(z,b_hat,
+        color='r',
+        label = 'Support Vector Regression')
+
+ax[0].legend()
+ax[0].set_ylabel('$\dot b$ (m a$^{-1}$)')
+ax[0].set_xlabel('Elevation (m a.s.l.)')
+
+# Make a colormap and all the associated var names
+cmap, norm, s_map, bounds = plotting.make_colorbar(mf_dataset)
+
+for delta_mb in Vol.Delta_MB:
+    color = cmap(norm(delta_mb))
+    ax[1].plot(Vol.t, Vol.sel(Delta_MB=delta_mb), color=color)
+
+ax[1].axhline(1.0,c='k',ls=':',lw=1.0)
+
+cbar = fig.colorbar(s_map,
+                spacing='proportional',
+                ticks=mf_dataset.Delta_MB,
+                ax=ax[1],
+                pad=0.01,
+                aspect=40,
+                boundaries=bounds,
+                drawedges=True,
+                format='%2.{}f'.format(2))
+# adjust x-axis limits
+ax[1].set_xlim(Vol.t.min(), Vol.t.max())
+# annotate the figures axes
+ax[1].set_ylabel('Relative Volume per Unit Width')
+ax[1].set_xlabel('Time (a)')
+# annotate the colorbar axes
+cbar.set_label('$\Delta \dot b$ (m a$^{-1}$)', rotation=270, labelpad=20)
+# cbar.ax.tick_params(labelsize='small')
 
 # Major ticks every 20, minor ticks every 5
 major_ticks = np.arange(0.8, 1.1, 0.05)
 minor_ticks = np.arange(0.8, 1.15, 0.01)
+ax[1].set_yticks(major_ticks);
+ax[1].set_yticks(minor_ticks, minor=True);
 
-title = r'\begin{center}'  + \
-        r'$2000 \; \rm{a}$ spin-up of Synthetic Glacier (Surging Tributary) \\'  + \
-        r'using a support vector regression of EMY $\dot b$ data (elevation dependent)'  + \
-        r' \end{center}'
 
-fig, ax = plotting.plot_volume(mf_dataset, precision=2) #, title=title
+ax[0].text(0.05, 0.9, 'a', transform=ax[0].transAxes,
+               fontsize=12, fontweight='bold')
+ax[1].text(0.05, 0.9, 'b', transform=ax[1].transAxes,
+               fontsize=12, fontweight='bold')
 
-ax.set_yticks(major_ticks);
-ax.set_yticks(minor_ticks, minor=True);
 
-fig.savefig('./mb_gridsearch_Vol.eps', facecolor='w', bbox_inches = 'tight', edgecolor='k')
+fig.savefig('mass_balance_supp.pdf')
+################################################################################
